8ae683593ed5dba6eaefdfb711e1a33f01199271
Prerona Borkotoky <40920728+PreronaB@users.noreply.github.com>
2020-12-27 21:04:43 +0530

    Followers repository

---
+++ FollowersRepository.java
@@ -0,0 +1,10 @@
+package com.cpg.pixogramspring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cpg.pixogramspring.entities.Followers;
+
+public interface FollowersRepository extends JpaRepository <Followers, Integer>{
+	
+
+}
